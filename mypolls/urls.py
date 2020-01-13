8675b763b266a66e41adefd92db930ddd68e4a5a
Plato <49398568+hellstaff27@users.noreply.github.com>
2020-01-14 00:29:56 +0300

    Add files via upload

---
+++ mypolls/urls.py
@@ -0,0 +1,29 @@
+"""mypolls URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('polls.urls')),
+    path('count/', include('count.urls')),
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
+handler404 = 'polls.views.handler404'
+# handler400 = 'polls.views.handler400'
+# handler500 = 'polls.views.handler500'
