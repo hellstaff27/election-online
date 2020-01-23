8d761d151fac81d2fa6bea5f787b808dd8beb07e
Wollermann <53054305+Wollermann@users.noreply.github.com>
2020-01-23 17:45:02 +0300

    fix

--- mypolls/settings.py
+++ mypolls/settings.py
@@ -86,9 +86,9 @@ DATABASES = {
      'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'polls',
-        'PASSWORD': 'Aueaueaue1!',
+        'PASSWORD': '',
         'USER': 'root',
-        'HOST': '194.67.105.192',
+        'HOST': 'localhost',
         'PORT': '8001',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
